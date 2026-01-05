12a82842e5d78b0a9d37e0a1f82ec6a132c8ae3f
Abdul Aziz Al Palembani <azizpetikdm@gmail.com>
2026-01-06 00:59:10 +0700

    Tugas-1 DDP Portofolio Website Abdul Aziz Al Palembani

---
+++ myweb/webku/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse    
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
